eeb0245b3e5b41e6f2e67bac6c05eb5a43e5b589
Dan D'Avella <drdavella@gmail.com>
2017-03-09 22:21:15 -0500

    Updated rotation code, new shapes

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,21 @@
 "use strict";
-const TETRIS_ROWS = 20;
+const TETRIS_ROWS = 24;
 const TETRIS_COLS = TETRIS_ROWS/2;
 const START_STEP_INTERVAL = 50;
-const NUM_SHAPES = 2;
+
+
+function createSquare(x, y, cellSide, topCorner, color) {
+
+    let square = new paper.Path.Rectangle({
+        point: [topCorner.x+1 + (cellSide*x), topCorner.y+1 + (cellSide*y)],
+        size: [cellSide-2, cellSide-2],
+        radius: 1,
+        strokeColor: 'black',
+        fillColor: color
+    });
+
+    return square;
+}
 
 
 function Stick(paper, cellSide, topCorner) {
@@ -11,46 +24,13 @@ function Stick(paper, cellSide, topCorner) {
     this.squares = new Array();
     for (let i = 0; i < 4; i++) {
         this.cells.push({x: i, y: 0});
-        this.squares.push(
-            paper.Path.Rectangle({
-                point: [topCorner.x+1 + (cellSide*i), topCorner.y+1],
-                size: [cellSide-2, cellSide-2],
-                radius: 1,
-                strokeColor: 'black',
-                fillColor: 'red'
-            })
-        );
+        this.squares.push(createSquare(i, 0, cellSide, topCorner, 'red'));
     }
 
     // choose a center point for the rotation
-    let centerIdx = 2;
-    let center = this.cells[centerIdx];
-
-    // Each shape needs to define its own rotate function
-    this.rotate = function() {
-        for (let i = 0; i < this.cells.length; i++) {
-            if (i == centerIdx) {
-                continue;
-            }
-
-            let cell = this.cells[i];
-            let square = this.squares[i];
-
-            let xDiff = cell.x - center.x;
-            let yDiff = cell.y - center.y;
-
-            // Apply 90 degree rotation matrix
-            let xOffset = -1*yDiff;
-            let yOffset = xDiff;
-
-            cell.x = center.x + xOffset;
-            cell.y = center.y + yOffset;
-
-            square.position.x = topCorner.x + cellSide*cell.x + cellSide/2;
-            square.position.y = topCorner.y + cellSide*cell.y + cellSide/2;
-
-        }
-    }
+    this.canRotate = true;
+    this.axisIdx = 2;
+    this.axis = this.cells[this.axisIdx];
 }
 
 function Square(paper, cellSide, topCorner) {
@@ -60,23 +40,60 @@ function Square(paper, cellSide, topCorner) {
     for (let i = 0; i < 2; i++) {
         for (let j = 0; j < 2; j++) {
             this.cells.push({x: i, y: j});
-            this.squares.push(
-                paper.Path.Rectangle({
-                    point: [topCorner.x+1 + (cellSide*i), topCorner.y+1 + (cellSide*j)],
-                    size: [cellSide-2, cellSide-2],
-                    radius: 1,
-                    strokeColor: 'black',
-                    fillColor: 'blue'
-                })
-            );
+            this.squares.push(createSquare(i, j, cellSide, topCorner, 'orange'));
         }
     }
 
-    // Each shape needs to define its own rotate function
-    this.rotate = function() {
-        // do nothing!
-        return;
+    this.canRotate = false;
+}
+
+function TBone(paper, cellSide, topCorner) {
+
+    this.cells = new Array();
+    this.squares = new Array();
+    for (let i = 0; i < 3; i++) {
+        this.cells.push({x: i, y: 0});
+        this.squares.push(createSquare(i, 0, cellSide, topCorner, 'teal'));
     }
+    this.cells.push({x: 1, y: 1});
+    this.squares.push(createSquare(1, 1, cellSide, topCorner, 'teal'));
+
+    // choose a center point for the rotation
+    this.canRotate = true;
+    this.axisIdx = 1;
+    this.axis = this.cells[this.axisIdx];
+}
+
+function Squiggle(paper, cellSide, topCorner) {
+
+    this.cells = new Array();
+    this.squares = new Array();
+    for (let i = 0; i < 2; i++) {
+        this.cells.push({x: 0, y: i});
+        this.squares.push(createSquare(0, i, cellSide, topCorner, 'lime'));
+        this.cells.push({x: 1, y: i+1});
+        this.squares.push(createSquare(1, i+1, cellSide, topCorner, 'lime'));
+    }
+
+    this.canRotate = true;
+    this.axisIdx = 1;
+    this.axis = this.cells[this.axisIdx];
+}
+
+function BendyGuy(paper, cellSide, topCorner) {
+
+    this.cells = new Array();
+    this.squares = new Array();
+    for (let i = 0; i < 3; i++) {
+        this.cells.push({x: 0, y: i});
+        this.squares.push(createSquare(0, i, cellSide, topCorner, 'purple'));
+    }
+    this.cells.push({x: 1, y: 0});
+    this.squares.push(createSquare(1, 0, cellSide, topCorner, 'purple'));
+
+    this.canRotate = true;
+    this.axisIdx = 0;
+    this.axis = this.cells[this.axisIdx];
 }
 
 
@@ -129,7 +146,7 @@ function Tetris(paper,winHeight) {
         return true;
     }
 
-    function moveShape(shape, xDirection, yDirection, cellSide) {
+    function moveShape(shape, xDirection, yDirection) {
         for (let square of shape.squares) {
             square.position.x += (xDirection * cellSide);
             square.position.y += (yDirection * cellSide);
@@ -137,6 +154,79 @@ function Tetris(paper,winHeight) {
         for (let cell of shape.cells) {
             cell.x += xDirection;
             cell.y += yDirection;
+        }
+    }
+
+    function getRotatedCells(shape) {
+        let xAdjust = 0;
+        let yAdjust = 0;
+        let newCells = new Array();
+        for (let i = 0; i < shape.cells.length; i++) {
+            // dont move our axis
+            if (i == shape.axisIdx) {
+                continue;
+            }
+
+            let cell = shape.cells[i];
+            let square = shape.squares[i];
+
+            let xDiff = cell.x - shape.axis.x;
+            let yDiff = cell.y - shape.axis.y;
+
+            // Apply 90 degree rotation matrix
+            let x = shape.axis.x + -1*yDiff;
+            let y = shape.axis.y + xDiff;
+
+            // make sure that our shape stays within bounds
+            if (x < 0 && (0 - x) >  xAdjust) {
+                xAdjust = 0 - x;
+            }
+            if (y < 0 && (0 - y) > yAdjust) {
+                yAdjust = 0 - y;
+            }
+            if (x >= TETRIS_COLS-1 && (TETRIS_COLS-1-x) < xAdjust) {
+                xAdjust = TETRIS_COLS - 1 - x;
+            }
+            if (y >= TETRIS_ROWS-1 && (TETRIS_ROWS-1-y) < yAdjust) {
+                yAdjust = TETRIS_ROWS - 1 - y;
+            }
+
+            newCells.push({x: x, y: y});
+        }
+
+        return {newCells: newCells, xAdjust: xAdjust, yAdjust: yAdjust};
+    }
+
+    function rotateShape(shape) {
+        if (!shape.canRotate) {
+            return;
+        }
+
+        let nc = getRotatedCells(shape);
+
+        // make sure this move is valid
+        for (let cell of nc.newCells) {
+            if (isCellOccupied(cell.x+nc.xAdjust, cell.y+nc.yAdjust)) {
+                return;
+            }
+        }
+
+        let j = 0;
+        for (let i = 0; i < shape.cells.length; i++) {
+            let square = shape.squares[i];
+
+            if (i == shape.axisIdx) {
+                shape.cells[i].x += nc.xAdjust;
+                shape.cells[i].y += nc.yAdjust;
+            }
+            else {
+                shape.cells[i].x = nc.newCells[j].x + nc.xAdjust;
+                shape.cells[i].y = nc.newCells[j].y + nc.yAdjust;
+                j++;
+            }
+
+            square.position.x = topCorner.x + cellSide*shape.cells[i].x + cellSide/2;
+            square.position.y = topCorner.y + cellSide*shape.cells[i].y + cellSide/2;
         }
     }
 
@@ -156,9 +246,9 @@ function Tetris(paper,winHeight) {
         return false;
     }
 
-    let shapes = new Array( Stick, Square );
+    let shapes = new Array( Stick, Square, TBone, Squiggle, BendyGuy);
     function shapeFactory() {
-        let index = Math.floor( Math.random() * NUM_SHAPES );
+        let index = Math.floor( Math.random() * shapes.length );
         return new shapes[index](paper, cellSide, topCorner);
     }
 
@@ -168,7 +258,7 @@ function Tetris(paper,winHeight) {
         if (activeShape != null) {
             if (event.count > 0 && event.count % stepInterval == 0) {
                 if (isValidMove(activeShape, 0, 1)) {
-                    moveShape(activeShape, 0, 1, cellSide);
+                    moveShape(activeShape, 0, 1);
                 }
                 else {
                     setCellsOccupied(activeShape.cells);
@@ -188,19 +278,19 @@ function Tetris(paper,winHeight) {
     let handleKey = function(event) {
         if (activeShape) {
             if (event.key == 'right' && isValidMove(activeShape, 1, 0)) {
-                moveShape(activeShape, 1, 0, cellSide);
+                moveShape(activeShape, 1, 0);
                 return false;
             }
             if (event.key == 'left' && isValidMove(activeShape, -1, 0)) {
-                moveShape(activeShape, -1, 0, cellSide);
+                moveShape(activeShape, -1, 0);
                 return false;
             }
             if (event.key == 'space') {
-                activeShape.rotate();
+                rotateShape(activeShape);
                 return false;
             }
             if (event.key == 'down' && isValidMove(activeShape, 0, 1)) {
-                moveShape(activeShape, 0, 1, cellSide);
+                moveShape(activeShape, 0, 1);
                 return false;
             }
         }
